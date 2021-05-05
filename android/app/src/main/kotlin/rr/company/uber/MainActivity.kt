b33c3b740dcc40533d601a39910cf0b131e25120
rafael.teixeira <rafael-kadu-ramon@hotmail.com>
2021-05-05 15:53:28 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/rr/company/uber/MainActivity.kt
@@ -0,0 +1,6 @@
+package rr.company.uber
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:uber/telas/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Uber',
+    home: Home(),
+  ));
+}
